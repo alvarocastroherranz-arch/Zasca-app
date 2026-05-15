b937fcd91179d20868d749a95ff15cfbb919441a
alvarocastroherranz-arch <alvarocastroherranz@gmail.com>
2026-05-16 01:08:20 +0200

    Update subscribe.js

--- api/subscribe.js
+++ api/subscribe.js
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
       headers: {
         'accept': 'application/json',
         'content-type': 'application/json',
-        'api-key': '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',
+        'api-key': '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',
       },
       body: JSON.stringify({ email, listIds: [2], updateEnabled: true }),
     });
